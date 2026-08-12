2d6f24ccd1ce5eee84b9f42e5287c1ec9a1821ba
jigar <jigarparmar9128@gmail.com>
2026-08-12 15:25:05 +0530

    update site

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://rajniwoodart.netlify.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rajniwoodart.netlify.app/review",
+      lastModified: new Date(),
+    },
+  ];
+}
