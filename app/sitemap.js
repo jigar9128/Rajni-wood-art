459e86fc9eceeb1cb47a471eaec59a08f49c8ec7
jigar <jigarparmar9128@gmail.com>
2026-08-12 15:32:46 +0530

    Fix sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,11 +1,15 @@
 export default function sitemap() {
   return [
     {
-      url: "https://rajniwoodart.netlify.app",
+      url: "https://rajniwoodart.netlify.app/",
       lastModified: new Date(),
     },
     {
       url: "https://rajniwoodart.netlify.app/review",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rajniwoodart.netlify.app/reviews",
       lastModified: new Date(),
     },
   ];
